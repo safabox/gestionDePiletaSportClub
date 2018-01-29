fb46b2beba2c87e7e0ed7f5947e15f603dcd8c52
facuobregon <facuobregon@gmail.com>
2018-01-29 11:27:43 -0300

    Add project files.

---
+++ gestionDePiletaSportClub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gestionDePiletaSportClub.Startup))]
+namespace gestionDePiletaSportClub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
