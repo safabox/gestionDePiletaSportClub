326c1a1f34c6cce6f8aec41a5889451bf3d60caf
facuobregon <facuobregon@gmail.com>
2018-01-30 07:55:55 -0300

    project setup, migrations enabled

---
+++ gestionDePiletaSportClub/App_Start/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+
+namespace gestionDePiletaSportClub.App_Start
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile() {
+
+        }
+    }
+}
--- gestionDePiletaSportClub/Global.asax.cs
+++ gestionDePiletaSportClub/Global.asax.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+using gestionDePiletaSportClub.App_Start;
+using AutoMapper;
+
+
 
 namespace gestionDePiletaSportClub
 {
@@ -12,6 +17,19 @@ namespace gestionDePiletaSportClub
     {
         protected void Application_Start()
         {
+
+            #region webApi
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
+    .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
+            GlobalConfiguration.Configuration.Formatters
+                .Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            #endregion
+
+            #region autoMapper
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            #endregion
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
