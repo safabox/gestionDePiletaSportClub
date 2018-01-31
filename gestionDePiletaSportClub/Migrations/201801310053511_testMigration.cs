438bb29815aeb9947e05656b949b108fc899fb0c
facuobregon <facuobregon@gmail.com>
2018-01-31 08:31:19 -0300

    update WebApiConfig for JSON formating on webapi calls

---
+++ gestionDePiletaSportClub/Migrations/201801310053511_testMigration.cs
@@ -0,0 +1,16 @@
+namespace gestionDePiletaSportClub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class testMigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
