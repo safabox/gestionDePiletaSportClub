1d54195aa96fdf69fd0cbc5acf5898c9bf882ee9
facuobregon <facuobregon@gmail.com>
2018-03-18 22:30:54 -0300

    cambios en moment-with-locales.js

--- gestionDePiletaSportClub/Scripts/moment-with-locales.js
+++ gestionDePiletaSportClub/Scripts/moment-with-locales.js
@@ -5033,17 +5033,20 @@
 (function (factory) {
     factory(moment);
 }(function (moment) {
-    var monthsShortDot = 'ene._feb._mar._abr._may._jun._jul._ago._sep._oct._nov._dic.'.split('_'),
+    //var monthsShortDot = 'ene._feb._mar._abr._may._jun._jul._ago._sep._oct._nov._dic.'.split('_'),
+        //monthsShort = 'ene_feb_mar_abr_may_jun_jul_ago_sep_oct_nov_dic'.split('_');
+    //FACU: cambios en moment-with-locales para el calendario
+    var monthsShortDot = 'ene_feb_mar_abr_may_jun_jul_ago_sep_oct_nov_dic'.split('_'),
         monthsShort = 'ene_feb_mar_abr_may_jun_jul_ago_sep_oct_nov_dic'.split('_');
 
     return moment.defineLocale('es', {
         months : 'enero_febrero_marzo_abril_mayo_junio_julio_agosto_septiembre_octubre_noviembre_diciembre'.split('_'),
         monthsShort : function (m, format) {
             if (/-MMM-/.test(format)) {
-                return monthsShortDot[m.month()];
-            } else {
                 return monthsShort[m.month()];
+            } else {
                 
+                return monthsShortDot[m.month()];
             }
         },
         weekdays : 'domingo_lunes_martes_miércoles_jueves_viernes_sábado'.split('_'),
