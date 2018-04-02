dab3bb6e5e4c4e60032f7eb973dcda4f3e7ba660
facuobregon <facuobregon@gmail.com>
2018-04-02 20:05:47 -0300

    solucion del bud de fechas y mostrar niveles segun edad del socio

---
+++ gestionDePiletaSportClub/Controllers/Api/MembershipsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using gestionDePiletaSportClub.DAL;
+using gestionDePiletaSportClub.Models;
+using gestionDePiletaSportClub.ViewModels;
+using gestionDePiletaSportClub.Dtos;
+using System.Data.Entity;
+using AutoMapper;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace gestionDePiletaSportClub.Controllers.Api
+{
+    public class MembershipsController : ApiController
+    {
+        private ApplicationDBContext _context;
+        private IMapper mapper;
+        public MembershipsController()
+        {
+            _context = ApplicationDBContext.Create();
+            mapper = Mapper.Instance;
+        }
+
+        public IEnumerable<MembershipTypeDto> GetMembershipTypes() {
+            var membershipTypes = _context.MembershipType.ToList().Select(Mapper.Map<MembershipType, MembershipTypeDto>); ;
+            return membershipTypes;
+        }
+
+        public MembershipTypeDto GetMembershipTypes(int Id)
+        {
+            var membershipType = _context.MembershipType.Where(m=> m.Id ==Id).SingleOrDefault(); 
+            return Mapper.Map<MembershipType, MembershipTypeDto>(membershipType);
+        }
+        [Route("api/Memberships/{Id}/Levels")]
+        public IEnumerable<LevelDto> GetLevelsForMembershipType(int Id) {
+            var levels = _context.MembershipType
+                .Where(m => m.Id == Id)
+                .SelectMany(m => m.Levels)
+                .Select(Mapper.Map<Level,LevelDto>);
+            return levels;
+
+        }
+
+
+
+    }
+}
